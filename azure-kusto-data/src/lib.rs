10c66ab42b55ac58d613dfbc78fda8c002cccba9
AsafMah <asafmahlev@microsoft.com>
2022-03-27 15:45:27 +0300

    Clippy error

--- azure-kusto-data/src/lib.rs
+++ azure-kusto-data/src/lib.rs
@@ -1,6 +1,10 @@
+fn nothing() {
+    let mut result = 3;
+}
+
 #[cfg(test)]
 mod tests {
-    #[test   ]
+    #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
